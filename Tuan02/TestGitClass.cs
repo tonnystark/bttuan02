57982829465f41b07cb9c426e647c8ac49232602
MUNI <thachmuni1996@gmail.com>
2017-08-26 20:09:13 +0700

    first commit

---
+++ Tuan02/TestGitClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tuan02
+{
+    class TestGitClass
+    {
+        //something in here
+    }
+}
